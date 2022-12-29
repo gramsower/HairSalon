063e58e6edfc78b2c9ef3bf38ffb52beecbea9e2
gramsower <110431307+gramsower@users.noreply.github.com>
2022-12-28 22:00:26 -0800

    adding controllers

---
+++ Controllers/StylistsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCoreCore;
+using Microsoft.AspNetCoreCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers;
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List
